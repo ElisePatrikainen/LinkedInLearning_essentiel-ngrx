da6ab9df01f1cdfb02a57510bb6dec3acd85341b
ElisePatrikainen <elise.patrikainen@gmail.com>
2020-12-05 09:21:45 +0100

    import dev tools

--- src/app/state/state.module.ts
+++ src/app/state/state.module.ts
@@ -1,3 +1,5 @@
+import { environment } from './../../environments/environment.prod';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -12,7 +14,11 @@ import { CommonModule } from '@angular/common';
       items: () => {},
       filters: () => {},
       selectedItems: () => {}
-    })
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ]
 })
 export class StateModule { }
