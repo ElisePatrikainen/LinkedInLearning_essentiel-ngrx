0de64cc17cbbcee2e818baf69e26ccbeab0aa9bc
ElisePatrikainen <elise.patrikainen@gmail.com>
2020-12-05 13:34:31 +0100

    fix

--- src/app/components/items/item/item.component.ts
+++ src/app/components/items/item/item.component.ts
@@ -1,18 +1,15 @@
 import { Item } from './../../../models/item';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent {
 
   @Input() item: Item;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
