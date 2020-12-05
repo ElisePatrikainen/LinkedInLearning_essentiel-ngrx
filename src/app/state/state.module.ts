063ed2a49b4b58ce3ced03c1ac8c525391be72c0
ElisePatrikainen <elise.patrikainen@gmail.com>
2020-12-05 08:52:58 +0100

    store import

---
+++ src/app/state/state.module.ts
@@ -0,0 +1,14 @@
+import { StoreModule } from '@ngrx/store';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StoreModule.forRoot()
+  ]
+})
+export class StateModule { }
